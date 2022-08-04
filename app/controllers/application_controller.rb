acb4d6b97158d4946402df0e7cc2990ddb17b093
Bertrand Bichat <bertrand.bichat@gmail.com>
2022-08-04 08:34:46 +0200

    custom navbar links + test current_user presence for layout + redirect_to index restaurants path after devise sign_in & sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  # redirect to index restaurants page after sign_in
+  def after_sign_in_path_for(resource)
+    restaurants_path
+  end
+
+  # redirect to index restaurants page after sign_up
+  def after_sign_up_path_for(resource)
+    restaurants_path
+  end
 end
